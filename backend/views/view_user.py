292b3c8611f45637401a9e459ad9588e15d61bc7
LithiraHettiarachchi <lithira.20200791@iit.ac.lk>
2023-04-06 09:40:49 +0530

    userpy

--- backend/views/view_user.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from controllers.user_controller import create_user, get_user, update_user, delete_user
-
-user_bp = Blueprint('user_bp', __name__)
-
-user_bp.route('/users', methods=['POST'])(create_user)
-user_bp.route('/users/<id>', methods=['GET'])(get_user)
-user_bp.route('/users/<id>', methods=['PUT'])(update_user)
-user_bp.route('/users/<id>', methods=['DELETE'])(delete_user)
